1df23263049c8fd9e137d44a342a0fe7dfefb2ab
jacopotatarelli <jacopo.11@hotmail.it>
2012-01-19 17:15:12 +0000

    primo progetto

---
+++ progetto1/src/pdm/jacopo/helloworld/Progetto1Activity.java
@@ -0,0 +1,12 @@
+package pdm.jacopo.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Progetto1Activity extends Activity {
+       
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
